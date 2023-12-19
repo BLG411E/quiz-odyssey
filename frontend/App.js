b2bd72e7d150cac0847f67d17a89ddd23eb82c13
Eren Taşdemir <29547193+ErenTD@users.noreply.github.com>
2023-12-19 19:44:09 +0300

    QO-5 Application Navigation and Auth Context

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useMemo, useReducer, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SecureStore from 'expo-secure-store';
+
+import AuthContext from './utils/AuthContext';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [state, dispatch] = useReducer(
+      (prevState, action) => {
+          switch (action.type) {
+              case ("RESTORE_TOKEN"):
+                  return {
+                      ...prevState,
+                      userToken: action.token,
+                      isLoading: false,
+                  };
+              case ("LOGIN"):
+                  return {
+                      ...prevState,
+                      isLogout: false,
+                      userToken: action.token,
+                  };
+              case ("LOGOUT"):
+                  return {
+                      ...prevState,
+                      isLogout: true,
+                      userToken: null,
+                  };
+          }
+      },
+      {
+          isLoading: true,
+          isLogout: false,
+          userToken: null,
+      }
+  );
+
+  useEffect(() => {
+      const bootstrapAsync = async () => {
+          let userToken;
+
+          try {
+              userToken = await SecureStore.getItemAsync("token");
+          } catch (e) {
+              console.error(e);
+          }
+
+          dispatch({ type: "RESTORE_TOKEN", token: userToken });
+      };
+
+      bootstrapAsync();
+  }, []);
+
+  const authContext = useMemo(
+      () => ({
+          SignIn: async (data) => {
+              const token = null // await Login(data); // TODO: Login function
+              dispatch({ type: "LOGIN", token: token });
+          },
+          Logout: async () => {
+              SecureStore.deleteItemAsync("token");
+              dispatch({ type: "LOGOUT" });
+          },
+          SignUp: async (data) => {
+              const token = null // await Register(data); // TODO: Register function
+              dispatch({ type: "LOGIN", token: token });
+          },
+      }),
+      []
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <AuthContext.Provider value={authContext}>
+          <NavigationContainer>
+              {/* // TODO: Write screen-specific navigator, include splash screen if loading */}
+          </NavigationContainer>
+      </AuthContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
