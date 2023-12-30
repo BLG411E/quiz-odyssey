958c9e7f411da563ab7f837ce6b2ebed53167d21
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2023-12-30 10:19:34 +0300

    Getting list of categories from backend added (GetCategories.js). category.py - backend endpoint works.

---
+++ backend/routes/category.py
@@ -0,0 +1,21 @@
+import datetime
+import werkzeug
+
+from flask import Blueprint, jsonify, request
+from sqlalchemy.exc import IntegrityError
+
+from .. import models
+from ..authutils import get_userID, get_staffID, require_token, require_moderator
+from ..extensions import db
+
+category = Blueprint("category", __name__)
+
+
+@category.route("/list", methods=["GET"])
+def list_categories():
+    # fetch all categories from database
+
+    categories = db.session.query(models.Category).all()
+    categories_list = [{"id": category.id, "name": category.name, "description": category.description}
+                       for category in categories]
+    return jsonify(categories_list)
