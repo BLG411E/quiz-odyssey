bd4faf30b93c762574bba4cd01709c4266dea922
ituitis-gjineci19 <gjineci19@itu.edu.tr>
2024-01-03 22:42:06 +0300

    Added movie data

--- backend/insert_data.py
+++ backend/insert_data.py
@@ -13,6 +13,7 @@ session = Session()
 
 def insert_questions():
     file_path = os.path.join(os.path.dirname(__file__), "music")
+    file_path2 = os.path.join(os.path.dirname(__file__), "movies")
 
     category_mapping = {
         "music": Category(name="Music"),  # Map file names to categories
@@ -93,6 +94,64 @@ def insert_questions():
     for question_data in questions:
         _, file_name = os.path.split(
             file_path
+        )  # Extract the file name or use it directly if already available
+        category_id = category_mapping.get(
+            file_name
+        ).id  # Get category ID from the mapping
+
+        correctAnswer = question_data["correctAnswer"]
+        if correctAnswer == question_data["option1"]:
+            correctAnswer = 1
+        elif correctAnswer == question_data["option2"]:
+            correctAnswer = 2
+        elif correctAnswer == question_data["option3"]:
+            correctAnswer = 3
+        elif correctAnswer == question_data["option4"]:
+            correctAnswer = 4
+
+        new_question = Question(
+            text=question_data["text"],
+            option1=question_data["option1"],
+            option2=question_data["option2"],
+            option3=question_data["option3"],
+            option4=question_data["option4"],
+            correctAnswer=correctAnswer,
+            addedBy=automation_user,
+            category=category_id,
+            isValid=True,  # Assuming all parsed questions are considered valid
+            approvedBy=automation_staff,
+            explanation="Explanation text",  # Replace with the explanation for the question
+            difficulty=1,  # Replace with the difficulty level of the question
+        )
+        session.add(new_question)
+        session.commit()
+
+    with open(file_path2, "r", encoding="latin-1") as file:
+        for line in file:
+            if lines_read >= lines_to_read:
+                break
+
+            line = line.strip()
+            if line.startswith("#Q"):
+                if current_question:
+                    questions.append(current_question)
+                current_question = {"text": line[3:].strip()}
+            elif line.startswith("^"):
+                current_question["correctAnswer"] = line[2:]
+            elif line.startswith("A "):
+                current_question["option1"] = line[2:]
+            elif line.startswith("B "):
+                current_question["option2"] = line[2:]
+            elif line.startswith("C "):
+                current_question["option3"] = line[2:]
+            elif line.startswith("D "):
+                current_question["option4"] = line[2:]
+
+            lines_read += 1
+
+    for question_data in questions:
+        _, file_name = os.path.split(
+            file_path2
         )  # Extract the file name or use it directly if already available
         category_id = category_mapping.get(
             file_name
