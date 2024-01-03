2df49c2da9caa40f6b7d2f3a327b6090ba6e9332
Eren Taşdemir <29547193+ErenTD@users.noreply.github.com>
2024-01-03 16:31:29 +0300

    QO-44 Add client for Quiz System WebSocket
    Co-authored-by: Berkant <chicane_timpani.0q@icloud.com>

--- frontend/pages/GameQuizPage/GameQuizPage.js
+++ frontend/pages/GameQuizPage/GameQuizPage.js
@@ -1,154 +1,200 @@
-import React, { useState, useContext, useEffect, route } from "react";
-import AuthContext from "../../utils/AuthContext";
+import React, { useEffect, useState, useReducer } from "react";
 import {
-  Text,
-  View,
-  Pressable,
-  ScrollView,
-  StatusBar,
+    Pressable,
+    StatusBar,
+    Text,
+    View
 } from 'react-native';
-import Slider from '@react-native-community/slider'
 import styles from "./styles";
+import { io } from "socket.io-client";
+import { API_URL } from "../../utils/AuthContext";
 
 
 const GameQuizPage = ({ route, navigation }) => {
-  const { token, categoryID, numberOfQuestions, currentQuestionNumber } = route.params;
+    const { token, categoryID, numberOfQuestions } = route.params;
 
-  const questionTime = 5;
-  const [timeLeft, setTimeLeft] = useState(questionTime);
-  const [ buttonColors, setButtonColors ] = useState(["white", "white", "white", "white"]);
-  
-  const changeButtonColor = (index1, color1, index2=-1, color2=-1) => {
-    setButtonColors((prevColors) => {
-      const newColors = [...prevColors];
-      newColors[index1] = color1;
-      if (index2 !== -1) {
-        newColors[index2] = color2;
-      }
-      return newColors;
+    const questionTime = 5; // 5 seconds
+    const [timeLeft, setTimeLeft] = useState(questionTime);
+    const [buttonColors, setButtonColors] = useState(["white", "white", "white", "white"]);
+    const [currentQuestion, setCurrentQuestion] = useState({
+        text: "NULL",
+        options: ["NULL", "NULL", "NULL", "NULL"],
     });
-  };
+    const [answerIndex, setAnswerIndex] = useState(0);
+    const [score, setScore] = useState(0);
+    const [currentQuestionNumber, setCurrentQuestionNumber] = useState(1);
+    const [userAnswer, setUserAnswer] = useState(0);
+    const [timer, setTimer] = useState(null);
 
-  const [isQuestionAnswered, setIsQuestionAnswered] = useState(false);
-  const breakAfterQuestion = 2;
+    const breakAfterQuestion = 2000; // 2 seconds (2000ms)
 
-  const questions = ["What is the capital of Germany",
-                    "What is the capital of France",
-                    "What is the capital of Italy",
-                    "What is the capital of Spain",
-                    "What is the capital of Portugal",];
-  const options = [["Berlin", "Munich"],
-                    ["Paris", "Marseille", "Lyon"],
-                    ["Milan", "Rome", "Naples", "Turin"],
-                    ["Seville", "Valencia", "Madrind"],
-                    ["Porto", "Coimbra", "Braga", "Lisbon"]];
-  const answers = ["Berlin", "Paris", "Rome", "Madrid", "Lisbon"];
-  const answerIndex = [0, 0, 1, 2, 3];
-
-  let timer;
-
-  useEffect(() => {
-    timer = setInterval(() => {
-      setTimeLeft((prevTime) => {
-        if (prevTime-1 === 0) {
-          clearInterval(timer);
-          handleAnswerSelection(-1, true);
-          return 0;
-        } else {
-          return prevTime - 1;
-        }
-      });
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
+    const changeButtonColor = (index, color) => {
+        setButtonColors((prevColors) => {
+            const newColors = [...prevColors];
+            newColors[index - 1] = color;
+            return newColors;
+        });
     };
-  }, []);
 
+    const [socket, setSocket] = useState(null);
 
+    const [quizState, dispatch] = useReducer((state, action) => {
+        switch (action.type) {
+            case "ANSWER":
+                socket.emit("answer", {
+                    answer: userAnswer,
+                });
+                changeButtonColor(userAnswer, "#F7F583");
+                clearInterval(timer);
+                return "PENDING_RESULT";
+            case "RECEIVED_RESULT":
+                if (userAnswer !== 0 && userAnswer !== answerIndex) {
+                    changeButtonColor(userAnswer, "#FF7F7F");
+                }
 
-  handleTimerEnd = () => {
-    changeButtonColor(answerIndex[currentQuestionNumber-1], "#D0FFBC");
-  }
+                if (userAnswer === 0) {
+                    changeButtonColor(answerIndex, "#F7F583");
+                } else {
+                    changeButtonColor(answerIndex, "#BFF783");
+                }
 
-  const handleAnswerSelection = async (index, isTimeUp=false) => {
-    if (isQuestionAnswered)
-      return;
-    
-    clearInterval(timer);
-    setTimeLeft(0);
-    setIsQuestionAnswered(true);
-
-    if (isTimeUp) {
-      changeButtonColor(answerIndex[currentQuestionNumber-1], "#fffdaf");
-    }
-    else {
-      if (index == answerIndex[currentQuestionNumber-1]) {
-        changeButtonColor(index, "#D0FFBC");
-      }
-      else if (index != answerIndex[currentQuestionNumber-1]) {
-        changeButtonColor(index, "#FF7F7F", answerIndex[currentQuestionNumber-1], "#D0FFBC");
-      }
-    }
-    
-    setTimeout(() => {
-      console.log("Current question number: " + currentQuestionNumber + ", Number of questions: " + numberOfQuestions);
-      if (currentQuestionNumber < numberOfQuestions) {
-        navigation.push('GameQuizPage', { token: token, categoryID: categoryID, 
-          numberOfQuestions: numberOfQuestions, currentQuestionNumber: currentQuestionNumber + 1});
-        return;
-      }
-      else {
-        navigation.navigate("MainPage", { token: token });
-      }
-    }, breakAfterQuestion * 1000);
-  }
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="default" />
-      <View style={styles.questionHeader}>
-        <Text style={styles.textHeader}>Question {currentQuestionNumber}/{numberOfQuestions}</Text>
-      </View>
-
-      <View style={styles.questionHolder}>
-        <Text style={styles.textQuestion}>{questions[currentQuestionNumber-1]}</Text> 
-      </View>
-
-      <View style={styles.circleTimer}>
-        <Text style={styles.textTimer}>{timeLeft >= 0 ? timeLeft : 0}</Text>
-      </View>
-
+                setTimeout(() => {
+                    dispatch({ type: "NEXT_QUESTION" });
+                }, breakAfterQuestion);
+                return "SHOWING_RESULT";
+            case "NEXT_QUESTION":
+                setButtonColors(["white", "white", "white", "white"]);
+                setCurrentQuestionNumber(currentQuestionNumber + 1);
+                socket.emit("next_question");
+                return "PENDING_QUESTION";
+            case "RECEIVED_QUESTION":
+                setTimeLeft(questionTime);
+                setUserAnswer(0);
+                const new_timer = setInterval(() => {
+                    setTimeLeft((prevTime) => {
+                        if (prevTime - 1 === 0) {
+                            clearInterval(new_timer);
+                            socket.emit("answer", {
+                                answer: 0,
+                            });
+                            return 0;
+                        } else {
+                            return prevTime - 1;
+                        }
+                    });
+                }, 1000);
+                setTimer(new_timer);
+                return "PENDING_ANSWER";
+            case "END":
+                socket.disconnect();
+                // Score is available at the "score" variable to display on the results page
+                // TODO: Change with results page
+                setTimeout(() => {
+                    navigation.navigate("MainPage", { token: token });
+                }, breakAfterQuestion);
+                return "DISCONNECTED";
+        }
+    }, "READY", () => {
+        const socket = io(`${API_URL}/quiz`, {
+            reconnectionDelayMax: 10000,
+            auth: {
+                "Token": global.token,
+            },
+        });
         
-        {options[currentQuestionNumber-1] && options[currentQuestionNumber-1].length >= 1 && (
-          <Pressable style={[styles.answerButton, { backgroundColor: buttonColors[0] }]} 
-          onPress={() => handleAnswerSelection(0)}>
-            <Text style={styles.textAnswer}>{options[currentQuestionNumber-1][0]}</Text>
-          </Pressable>
-        )}
+        socket.emit("start", {
+            category: categoryID,
+            question_count: numberOfQuestions
+        });
 
-        {options[currentQuestionNumber-1].length >= 2 && (
-          <Pressable  style={[styles.answerButton, { backgroundColor: buttonColors[1] }]}
-          onPress={() => handleAnswerSelection(1)}>
-            <Text style={styles.textAnswer}>{options[currentQuestionNumber-1][1]}</Text>
-          </Pressable>
-        )}
+        setSocket(socket);
+        return "PENDING_ANSWER";
+    });
 
-        {options[currentQuestionNumber-1].length >= 3 && (
-          <Pressable  style={[styles.answerButton, { backgroundColor: buttonColors[2] }]}
-          onPress={() => handleAnswerSelection(2)}>
-            <Text style={styles.textAnswer}>{options[currentQuestionNumber-1][2]}</Text>
-          </Pressable>
-        )}
+    useEffect(() => {
+        socket.on("error", (error) => {
+            console.log("Error: ", error);
+        });
 
-        {options[currentQuestionNumber-1].length === 4 && (
-          <Pressable style={[styles.answerButton, { backgroundColor: buttonColors[3] }]} onPress={() => handleAnswerSelection(3)}>
-            <Text style={styles.textAnswer}>{options[currentQuestionNumber-1][3]}</Text>
-          </Pressable>
-        )}
+        socket.on("new_question", (question) => {
+            setCurrentQuestion(question);
+            dispatch({ type: "RECEIVED_QUESTION" });
+        });
 
-    </View>
-  );
+        socket.on("result", (data) => {
+            // Highlight answer on frontend, wait for next question
+            setAnswerIndex(data.correct_answer);
+            setScore(data.score);
+            dispatch({ type: "RECEIVED_RESULT" });
+        });
+
+        socket.on("end", (score, question_count) => {
+            // Display score on frontend
+            setScore(score);
+            dispatch({ type: "END" });
+        });
+
+        return () => {
+            socket.off("error");
+            socket.off("new_question");
+            socket.off("result");
+            socket.off("end");
+        }
+    }, []);
+
+    const handleAnswerSelection = async (index, isTimeUp = false) => {
+        if (quizState !== "PENDING_ANSWER") return;
+        setTimeLeft(0);
+        setUserAnswer(index);
+        dispatch({ type: "ANSWER" });
+    }
+
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="default" />
+            <View style={styles.questionHeader}>
+                <Text style={styles.textHeader}>Question {currentQuestionNumber}/{numberOfQuestions}</Text>
+            </View>
+
+            <View style={styles.questionHolder}>
+                <Text style={styles.textQuestion}>{currentQuestion["text"]}</Text>
+            </View>
+
+            <View style={styles.circleTimer}>
+                <Text style={styles.textTimer}>{timeLeft >= 0 ? timeLeft : 0}</Text>
+            </View>
+
+
+            {currentQuestion["options"] && currentQuestion["options"].length >= 1 && (
+                <Pressable style={[styles.answerButton, { backgroundColor: buttonColors[0] }]}
+                    onPress={() => handleAnswerSelection(1)}>
+                    <Text style={styles.textAnswer}>{currentQuestion["options"][0]}</Text>
+                </Pressable>
+            )}
+
+            {currentQuestion["options"] && currentQuestion["options"].length >= 2 && (
+                <Pressable style={[styles.answerButton, { backgroundColor: buttonColors[1] }]}
+                    onPress={() => handleAnswerSelection(2)}>
+                    <Text style={styles.textAnswer}>{currentQuestion["options"][1]}</Text>
+                </Pressable>
+            )}
+
+            {currentQuestion["options"] && currentQuestion["options"].length >= 3 && (
+                <Pressable style={[styles.answerButton, { backgroundColor: buttonColors[2] }]}
+                    onPress={() => handleAnswerSelection(3)}>
+                    <Text style={styles.textAnswer}>{currentQuestion["options"][2]}</Text>
+                </Pressable>
+            )}
+
+            {currentQuestion["options"] && currentQuestion["options"].length === 4 && (
+                <Pressable style={[styles.answerButton, { backgroundColor: buttonColors[3] }]}
+                    onPress={() => handleAnswerSelection(4)}>
+                    <Text style={styles.textAnswer}>{currentQuestion["options"][3]}</Text>
+                </Pressable>
+            )}
+
+        </View>
+    );
 };
 
 export default GameQuizPage;
