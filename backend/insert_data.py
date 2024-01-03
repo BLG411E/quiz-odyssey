ef2cc9659f5797c272d977ccd5ed1f71fa43b096
Berkant <chicane_timpani.0q@icloud.com>
2024-01-03 22:15:46 +0300

    QO-42 Fix insert_questions function

--- backend/insert_data.py
+++ backend/insert_data.py
@@ -1,30 +1,73 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from .models import Question
 import os
 
+from sqlalchemy import create_engine, select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
+
+from .models import Category, Question, Staff, User
 
 engine = create_engine(os.getenv("QUIZODYSSEY_DATABASE_URI"))
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 def insert_questions():
-    file_path = os.path.join(os.path.dirname(__file__), 'data/music')
+    file_path = os.path.join(os.path.dirname(__file__), "music")
 
     category_mapping = {
-        "music": 1,  # Map file names to category IDs
-        "movies": 2,
-    }        
+        "music": Category(name="Music"),  # Map file names to categories
+        "movies": Category(name="Movies"),
+    }
+
+    session.add_all(category_mapping.values())
+
+    for category in category_mapping.values():
+        try:
+            session.add(category)
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+
+    categories = session.execute(select(Category)).fetchall()
+    category_mapping = {
+        category[0].name.lower(): category[0] for category in categories
+    }
+
+    # Create a dummy staff member and user to use for adding and approving questions
+    automation_staff = session.execute(
+        select(Staff.id).where(Staff.username == "automation")
+    ).fetchone()[0]
+    if not automation_staff:
+        automation_staff = Staff(username="automation", passwordHash="INVALID")
+        session.add(automation_staff)
+        session.commit()
+        session.refresh(automation_staff)
+        automation_staff = automation_staff.id
+
+    automation_user = session.execute(
+        select(User.id).where(User.username == "automation")
+    ).fetchone()[0]
+    if not automation_user:
+        automation_user = User(
+            username="automation",
+            passwordHash="INVALID",
+            email="automation@quizodyssey.com",
+        )
+        session.add(automation_user)
+        session.commit()
+        session.refresh(automation_user)
+        automation_user = automation_user.id
+
     lines_to_read = 141
     lines_read = 0
     questions = []
     current_question = None
 
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
             if lines_read >= lines_to_read:
                 break
-            
+
             line = line.strip()
             if line.startswith("#Q"):
                 if current_question:
@@ -40,12 +83,26 @@ def insert_questions():
                 current_question["option3"] = line[2:]
             elif line.startswith("D "):
                 current_question["option4"] = line[2:]
-            
+
             lines_read += 1
 
     for question_data in questions:
-        file_name = file_path  # Extract the file name or use it directly if already available
-        category_id = category_mapping.get(file_name)  # Get category ID from the mapping
+        _, file_name = os.path.split(
+            file_path
+        )  # Extract the file name or use it directly if already available
+        category_id = category_mapping.get(
+            file_name
+        ).id  # Get category ID from the mapping
+
+        correctAnswer = question_data["correctAnswer"]
+        if correctAnswer == question_data["option1"]:
+            correctAnswer = 1
+        elif correctAnswer == question_data["option2"]:
+            correctAnswer = 2
+        elif correctAnswer == question_data["option3"]:
+            correctAnswer = 3
+        elif correctAnswer == question_data["option4"]:
+            correctAnswer = 4
 
         new_question = Question(
             text=question_data["text"],
@@ -53,15 +110,16 @@ def insert_questions():
             option2=question_data["option2"],
             option3=question_data["option3"],
             option4=question_data["option4"],
-            correctAnswer=question_data["correctAnswer"],
-            addedBy=1,  # Replace with the ID of the user who added the question
+            correctAnswer=correctAnswer,
+            addedBy=automation_user,
             category=category_id,
             isValid=True,  # Assuming all parsed questions are considered valid
-            approvedBy=1,  # Replace with the ID of the staff member who approved the question
+            approvedBy=automation_staff,
             explanation="Explanation text",  # Replace with the explanation for the question
-            difficulty=3  # Replace with the difficulty level of the question
+            difficulty=1,  # Replace with the difficulty level of the question
         )
         session.add(new_question)
+        session.commit()
 
-session.commit()
+
 session.close()
