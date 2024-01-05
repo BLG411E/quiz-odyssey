decd35fac2349522d87b49534eba8e0fc72449ef
Pijus-N <75778499+Pijus-N@users.noreply.github.com>
2024-01-05 15:25:10 +0300

    Pagination of leaderboards table

--- frontend/pages/LeaderboardPage.js
+++ frontend/pages/LeaderboardPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, TouchableOpacity, View, Button } from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list';
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from '../styles';
@@ -9,95 +9,121 @@ import GetCategories from '../utils/GetCategories';
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const LeaderboardPage = ({ route, navigation }) => {
-    const [userList, setUserList] = useState([]);
-    const [data,setData] = React.useState([]);
-    const [userData,setUserData] = React.useState([]);
-    const [selected, setSelected] = useState("");
-    const { token } = route.params
-    const getUserDataFromDatabase = async () => {
-        const users = await GetAllUserScoreData();
-        const userData = await GetUserInfo(token);
- 
-        setUserList(users["results"])
-        setUserData(userData);
+  const [userList, setUserList] = useState([]);
+  const [data, setData] = useState([]);
+  const [userData, setUserData] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10); // You can adjust the number of items per page
+
+  const { token } = route.params;
+
+  const getUserDataFromDatabase = async () => {
+    const users = await GetAllUserScoreData();
+    const userData = await GetUserInfo(token);
+
+    setUserList(users["results"]);
+    setUserData(userData);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await getUserDataFromDatabase();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await getUserDataFromDatabase();
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-          try {
-            const categories = await GetCategories();
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const categories = await GetCategories();
 
-            let newArray = categories.map((item) => {
-                return {key: item[0], value: item[1]}
-              })
-              setData(newArray);
-   
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        fetchCategories();
-      }, []);
+        let newArray = categories.map((item) => {
+          return { key: item[0], value: item[1] };
+        });
+        setData(newArray);
 
-      const fetchLeaderboardData = async (category) => {
-        // const leaderboardData = await GetLeaderboardData(category);
-        // setUserList(leaderboardData.results);
-        console.log(category);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-      useEffect(() => {
-        // Fetch leaderboard data when the selected category changes
-        if (selected) {
-            const selectedCategoryIndex =  data.find(item => item.value === selected);
-            fetchLeaderboardData(selectedCategoryIndex["key"]);
-        }
-    }, [selected]);
+    fetchCategories();
+  }, []);
 
-    const renderUser = ({ item, index }) => (
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10, backgroundColor: 'white',
-         backgroundColor: item.username === userData["username"] ? '#ffc15e' : 'white', }}>
-            <Text style={{ fontSize: 18 }}>{`${index + 1}. ${item.username}`}</Text>
-            <Text style={{ fontSize: 18 }}>{`Score: ${item.score}`}</Text>
+  const fetchLeaderboardData = async (category) => {
+    // const leaderboardData = await GetLeaderboardData(category);
+    // setUserList(leaderboardData.results);
+    console.log(category);
+  };
+
+  useEffect(() => {
+
+    if (selected) {
+      const selectedCategoryIndex = data.find(item => item.value === selected);
+      fetchLeaderboardData(selectedCategoryIndex["key"]);
+    }
+  }, [selected]);
+
+  const renderUser = ({ item, index }) => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    if (index >= startIndex && index < endIndex) {
+      return (
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10, backgroundColor: 'white', backgroundColor: item.username === userData["username"] ? '#ffc15e' : 'white', }}>
+          <Text style={{ fontSize: 18 }}>{`${index + 1}. ${item.username}`}</Text>
+          <Text style={{ fontSize: 18 }}>{`Score: ${item.score}`}</Text>
         </View>
-    );
+      );
+    }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.profileHeader}>
-                <Icon.Button backgroundColor="rgba(0,0,0,0)" name="chevron-back-outline" size={30} color="white" iconStyle={{marginRight: 0}} onPress={() => {
-                    navigation.navigate('MainPage');
-                }}/>
-                <Text style={styles.profileHeaderText}>{"Leaderboards"}</Text>
-            </View>
+    return null;
+  };
 
-            <View style={{ padding: 10 }}>
-                <View style={{ paddingBottom: 10 }}>
-                    <SelectList style={{ backgroundColor: "#000", textColor: 'white' }}
-                        textColor="white"
-                        setSelected={(val) => setSelected(val)}
-                        data={data}
-                        save="value"
-                    />
-                </View>
-                <FlatList
-                    data={userList}
-                    keyExtractor={(item) => item.username}
-                    renderItem={renderUser}
-                />
-            </View>
-        </SafeAreaView>
-    )
+  const handleNextPage = () => {
+    const totalPages = Math.ceil(userList.length / itemsPerPage);
+    setCurrentPage(currentPage + 1 > totalPages ? totalPages : currentPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1 < 1 ? 1 : currentPage - 1);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.profileHeader}>
+        <Icon.Button backgroundColor="rgba(0,0,0,0)" name="chevron-back-outline" size={30} color="white" iconStyle={{ marginRight: 0 }} onPress={() => {
+          navigation.navigate('MainPage');
+        }} />
+        <Text style={styles.profileHeaderText}>{"Leaderboards"}</Text>
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <View style={{ paddingBottom: 10 }}>
+          <SelectList style={{ backgroundColor: "#000", textColor: 'white' }}
+            textColor="white"
+            setSelected={(val) => setSelected(val)}
+            data={data}
+            save="value"
+          />
+        </View>
+        <FlatList
+          data={userList}
+          keyExtractor={(item) => item.username}
+          renderItem={renderUser}
+        />
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Button title="Previous Page" onPress={handlePrevPage} disabled={currentPage === 1} />
+          <Button title="Next Page" onPress={handleNextPage} disabled={currentPage * itemsPerPage >= userList.length} />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 };
 
 export default LeaderboardPage;
