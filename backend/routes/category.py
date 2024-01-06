c689206a037c4680abb53c2d62795db64ad3e7f8
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2024-01-06 15:54:50 +0300

    Update category.py

--- backend/routes/category.py
+++ backend/routes/category.py
@@ -13,9 +13,10 @@ category = Blueprint("category", __name__)
 
 @category.route("/list", methods=["GET"])
 def list_categories():
-    # fetch all categories from database
-
-    categories = db.session.query(models.Category).all()
-    categories_list = [{"id": category.id, "name": category.name, "description": category.description}
-                       for category in categories if category.name != "Daily"]
-    return jsonify(categories_list)
+    try:
+        categories = db.session.query(models.Category).all()
+        categories_list = [{"id": category.id, "name": category.name, "description": category.description}
+                           for category in categories if category.name != "Daily"]
+        return jsonify(categories_list), 200
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 500
